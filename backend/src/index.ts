47518ebe9485c7e289c7980deff91cd625362029
Dushan_indunil <dushanindunil63@gmail.com>
2025-11-23 23:24:03 +0530

    Products and Orders

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,8 +5,18 @@ const port = 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+});
+
+app.get('/products', (req, res) => {
+    res.send("The list of Products")
+});
+
+app.post('/products', (req, res) => {
+    res.send("add new Products")
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+// next   react-native   express 
+// https://www.youtube.com/watch?v=RDM-nk5f4SE&list=PLWbd8KS5ML2m1voyl0cEa58mC8fDV0tCe
